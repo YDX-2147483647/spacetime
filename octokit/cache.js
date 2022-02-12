87201723d4ef63e21828b8c834d06ab49063b5d2
Y.D.X <73375426+YDX-2147483647@users.noreply.github.com>
2022-02-12 22:25:29 +0800

    fix regression
    - [Private class fields](https://caniuse.com/mdn-javascript_classes_private_class_fields)
    - [Private class methods](https://caniuse.com/mdn-javascript_classes_private_class_methods)

--- octokit/cache.js
+++ octokit/cache.js
@@ -2,36 +2,33 @@
  * @see https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Map/
  */
 export default class Cache {
-    /** @type {Storage} */
-    #storage
-    /** @type {String} */
-    #key_in_storage
-    /** @type {Map<String, *>} */
-    #cache
 
     /**
      * @param {Storage} storage `localStorage`
      * @param {String} key_in_storage namespace
      */
     constructor(storage, key_in_storage) {
-        this.#storage = storage
-        this.#key_in_storage = key_in_storage
+        /** @type {Storage} */
+        this._storage = storage
+        /** @type {String} */
+        this._key_in_storage = key_in_storage
 
         const raw = storage.getItem(key_in_storage)
-        this.#cache = new Map(JSON.parse(raw))
+        /** @type {Map<String, *>} */
+        this._cache = new Map(JSON.parse(raw))
     }
 
     /**
      * @param {String} key 
      */
     get(key) {
-        return this.#cache.get(key)
+        return this._cache.get(key)
     }
     /**
      * @param {String} key 
      */
     has(key) {
-        return this.#cache.has(key)
+        return this._cache.has(key)
     }
 
     /**
@@ -41,8 +38,8 @@ export default class Cache {
      * @returns {Cache} this
      */
     set(key, value) {
-        this.#cache.set(key, value)
-        this.#save()
+        this._cache.set(key, value)
+        this._save()
 
         return this
     }
@@ -52,20 +49,20 @@ export default class Cache {
      * @returns {Boolean} if existed and has been removed
      */
     delete(key) {
-        const existed_and_removed = this.#cache.delete(key)
-        this.#save()
+        const existed_and_removed = this._cache.delete(key)
+        this._save()
         return existed_and_removed
     }
 
     clear() {
-        this.#cache.clear()
-        this.#save()
+        this._cache.clear()
+        this._save()
     }
 
-    #save() {
-        const literal = Array.from(this.#cache.entries())
-        this.#storage.setItem(
-            this.#key_in_storage,
+    _save() {
+        const literal = Array.from(this._cache.entries())
+        this._storage.setItem(
+            this._key_in_storage,
             JSON.stringify(literal)
         )
     }
